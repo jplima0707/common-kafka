f875e5718fea4bac706d1e3f48df26cd607e0256
unknown <jpbenevideslima0707@gmail.com>
2026-01-10 14:07:09 -0300

    first commit

---
+++ src/test/java/com/example/common_kafka/CommonKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
