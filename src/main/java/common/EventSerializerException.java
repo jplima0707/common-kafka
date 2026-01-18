65ec364257faa2930ba12cf1ef6d08b9ee6d7a5e
unknown <jpbenevideslima0707@gmail.com>
2026-01-18 20:17:06 -0300

    event serializer and exception

---
+++ src/main/java/common/EventSerializerException.java
@@ -0,0 +1,9 @@
+package common;
+
+public class EventSerializerException extends RuntimeException {
+
+    public EventSerializerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
